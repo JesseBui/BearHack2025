34a6b06926e5c49dec7b14a4f6e8672341aeb503
Carlo Maximo <maximocgc@gmail.com>
2025-03-30 09:35:53 -0400

    added front

--- front/src/app/mood-market-stock/mood-market-stock.component.ts
+++ front/src/app/mood-market-stock/mood-market-stock.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+// stock-recommendation.component.ts
+import { Component, OnInit } from '@angular/core';
+import { MoodMarketService, OutputMoodMarket } from '../mood-market.service'; // Import the service
+import { StockRecommendationService } from '../mood-market-recommendation.service';// Assuming you have a StockService
 
 @Component({
   selector: 'app-mood-market-stock',
-  imports: [],
   templateUrl: './mood-market-stock.component.html',
-  styleUrl: './mood-market-stock.component.css'
+  styleUrls: ['./mood-market-stock.component.css']
 })
-export class MoodMarketStockComponent {
+export class StockRecommendationComponent implements OnInit {
+  moodMarketData: OutputMoodMarket | null = null;
+  recommendations: any; // Assuming recommendations can be of any type
 
+  constructor(private moodMarketService: MoodMarketService, private stockService: StockRecommendationService) { }
+
+  ngOnInit() {
+    this.moodMarketService.currentMoodMarketData.subscribe(data => {
+      this.moodMarketData = data;
+      if (this.moodMarketData) {
+        // Use the moodMarketData to fetch stock recommendations
+        this.fetchRecommendations(this.moodMarketData.moodMarketEmotion);
+      }
+    });
+  }
+
+  fetchRecommendations(emotion: string) {
+    this.stockService.getStockRecommendationsByMood(emotion).subscribe({
+      next: (data) => {
+        this.recommendations = data;
+      },
+      error: (error) => {
+        console.error('Error fetching recommendations:', error);
+        this.recommendations = 'Failed to fetch recommendations.';
+      },
+      complete: () => {
+        console.log('Recommendations fetched successfully.');
+      }
+    });
+  }
 }
